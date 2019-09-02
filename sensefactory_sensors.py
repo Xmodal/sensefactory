9b5a86eefd46f47905464da0294b35c3c9630129
Sofian Audry <info@sofianaudry.com>
2019-09-02 17:52:13 +0200

    Started adding room counting features.

--- sensefactory_sensors.py
+++ sensefactory_sensors.py
@@ -7,6 +7,27 @@ from pythonosc import osc_server
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
+class Room:
+    def __init__(self, id):
+        self._id = id
+        self.count = 0
+
+    def id(self):
+        return self._id
+
+    def getCount(self):
+        return self.count
+
+    def setCount(self, count):
+        if count < 0:
+            count = 0
+        self.count = count
+
+    def add(self, count):
+        self.setCount(self.count + count)
+
+BASE_DISTANCE_THRESHOLD = 0.9
+
 class NodeSignal:
 
     def __init__(self, id, roomId, base_distance):
@@ -24,6 +45,8 @@ class NodeSignal:
     def id(self):
         return self._id
 
+    def roomId(self):
+        return self._roomId
 
     def update(self, t, distance):
         detected = False
@@ -74,20 +97,68 @@ verbose_mode = args.verbose
 next_data_requested = False
 start_time = time.time()
 
-# Create node signal objects.
+# Create data objects.
 node_signals = {}
-for i in range(1, 8):
-    node_signals[i] = NodeSignal(i)
+rooms = {}
+
+# Adds a node signal.
+def add_node(nodeId, roomId, base_distance):
+    global node_signals
+    node_signals[nodeId] = NodeSignal(nodeId, roomId, base_distance)
+
+def add_room(roomId):
+    global rooms
+    rooms[roomId] = Room(roomId)
+
+# Create all nodes.
+add_node(1, 1, 65535)
+add_node(2, 1, 65535)
+add_node(3, 2, 65535)
+add_node(4, 2, 50)
+add_node(5, 3, 65535)
+add_node(6, 3, 65535)
+
+add_node(7, 0, 65535)
+
+add_node(8, 0, 65535) # dummy
+
+OUTDOOR_ROOM_ID = 0
+LAST_ROOM_ID = 3
+
+# Create all rooms.
+add_room(0) # Room 0 corresponds to the outdoor
+add_room(1)
+add_room(2)
+add_room(3)
 
 # Re-route action to robot.
 def receive_data(unused_addr, nid, distance, strength, integration):
     global node_signals, start_time, client
-    t = time.time() - start_time
-    detected = node_signals[nid].update(t, distance)
-    if detected:
-        client.send_message("/sensefactory/sensor/detect", [ nid ])
 
-#def receive_rssi(unused_addr, nid, rssi):
+    t = time.time() - start_time
+
+    node = node_signals[nid]
+    detected = node.update(t, distance)
+
+    if detected:
+        # Trigger information about detection.
+        client.send_message("/sensefactory/sensor/detect", [ nid, detected ])
+
+        # Update counts.
+        roomId = node.roomId()
+        prevRoomId = roomId - 1
+        if prevRoomId < 0:
+            prevRoomId = LAST_ROOM_ID
+        # Person moves from previous room to the next room.
+        rooms[prevRoomId].add(-1)
+        rooms[roomId].add(1)
+
+
+def send_stats():
+    totalCount = rooms[1].getCount() + rooms[2].getCount() + rooms[3].getCount()
+    client.send_message("/sensefactory/rooms/counts" [ rooms[1].getCount(), rooms[2].getCount(), rooms[3].getCount(), totalCount ])
+
+
 
 dispatcher.map("/minibee/data", receive_data)
 server_thread.start()
